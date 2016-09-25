b863b0803d6211549cbc593b28e52d5b1ddb008e
Paul Carnibella <paulcarnibella@gmail.com>
2016-09-25 12:54:54 +1000

    More mobile responsive fixes

--- scripts/responsive-fixes.js
+++ scripts/responsive-fixes.js
@@ -3,7 +3,9 @@
 (function () {
   var additionalStyles = document.createElement('style')
   var $navbarHeader = $('.navbar-header')
+  var $navbarBrand = $('.navbar-brand')
   var $navbarMenu = $('.header-customer-asset')
+  var $navbarMobileMenu = $('#showLeftPushMenu')
   var $searchBar = $('.header-search-boxwrapper')
   var $shoppingChoices = $('.shopping-choices')
   var $shoppingChoicesItems = $shoppingChoices.find('.list-group-item')
@@ -43,6 +45,44 @@
         '}')
     },
 
+    navbarMenuInsertIntoHeader: function() {
+      var $navbarMenuClone = $navbarMenu.clone()
+      $navbarMenuClone.addClass('navbar-menu-clone')
+      $navbarMenuClone.insertAfter($navbarBrand)
+      $navbarMobileMenu.insertAfter($navbarBrand)
+
+      $(additionalStyles).append(
+        '.navbar-menu-clone { display:none; }' +
+        '@media screen and (max-width: 767px) {' +
+          '.navbar-menu-clone { display:block !important; }' +
+        '}')
+    },
+
+    navbarMobileColor: function() {
+      $(additionalStyles).append(
+        '@media screen and (max-width: 767px) {' +
+          '.navbar-header { border-bottom: 5px solid #ff3f33 }' +
+        '}')
+    },
+
+    navbarMobileRemoveNearestStore: function() {
+      $('#div__header .hidden-sm.hidden-md.hidden-lg a[href="/Find-Store"]').hide()
+    },
+
+    navbarMobileMenu: function() {
+      $(additionalStyles).append(
+        '@media screen and (max-width: 767px) {' +
+          '#showLeftPushMenu { margin-top: 12px; }' +
+          '.header-customer-asset { float: right !important; }' +
+          '.navbar-brand { float: left !important; }' +
+          '.my-account-header-link, .shopping-cart-header-icon { zoom: 0.8; margin-top: 10px; }' +
+          '.shopping-cart-header-icon span { margin-left: 4px; }' +
+          '.sign-in-header, .mycart-header-link { display: none !important; }' + 
+          '.header-customer-asset { width: 120px; padding: 0; }' +
+          '.navbar-brand { width: 135px; padding: 0; height: auto; margin-top: 4px; }' +
+        '}')
+    },
+
     addStyles: function() {
       $('body').append(additionalStyles)
     }
@@ -52,5 +92,9 @@
   MANGLORD.navbarMenuFixWidth()
   MANGLORD.searchBarFixPadding()
   MANGLORD.carouselTablet()
+  MANGLORD.navbarMobileColor()
+  MANGLORD.navbarMenuInsertIntoHeader()
+  MANGLORD.navbarMobileRemoveNearestStore()
+  MANGLORD.navbarMobileMenu()
   MANGLORD.addStyles()
 })()
